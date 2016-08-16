bbf19a138287628a0a64d198933988ae4dce5852
Bijesse <tom@bijesse.com>
2016-08-16 16:24:04 -0400

    typos

--- GA_JSD/feedr/js/app.js
+++ GA_JSD/feedr/js/app.js
@@ -12,11 +12,11 @@ var dropdown = ["Digg", "Mashable", "Reddit"];
     $('#drop').append('<li id=' + element + '><a href = "">' + element + '</a></li>');
     $('#' + element).click(function(e) {
       e.preventDefault();
-      open[index](); 
+      open[index]();
       $('span').html(element);
-    })
+    });
 
-    
+
   });
 
 //  Templating solution for appending 3 sources
@@ -35,9 +35,9 @@ var dropdown = ["Digg", "Mashable", "Reddit"];
   //})
 
 
-var diggRequest = "https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json"
-var mashRequest = "https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json"
-var redditRequest = "https://www.reddit.com/top.json"
+var diggRequest = "https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json";
+var mashRequest = "https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json";
+var redditRequest = "https://www.reddit.com/top.json";
 
 function openDigg(){
   $.ajax(
@@ -52,11 +52,11 @@ function openDigg(){
           var article_struct = '<article class="article">';
           var article_pic = '<section class="featuredImage">' +'<img src="'+articleObj.content.media.images[0].url+'" alt="" />' + '</section>';
           var article_content = '<section class="articleContent"> <a href ="'+articleObj.content.original_url+'"><h3>'+ index + ' ' + articleObj.content.title_alt+'</h3></a><h6>'+articleObj.content.domain+'</h6></section>';
-          var impressions = '<section class="impressions">' + articleObj.diggs.count + '</section>'
+          var impressions = '<section class="impressions">' + articleObj.diggs.count + '</section>';
           $('#main').append(article_struct + article_pic + article_content+impressions+'<div class="clearfix"></div></article>');
         });
        // $("ul").append(response.data.feed[0].content.title_alt);
-      
+
       },
   });
 }
@@ -73,7 +73,7 @@ function openMash(){
         var article_struct = '<article class="article">';
         var article_pic = '<section class="featuredImage">' +'<img src="'+articleObj.responsive_images[0].image+'" alt="" />' + '</section>';
         var article_content = '<section class="articleContent"> <a href ="'+articleObj.link+'"><h3>'+ index + ' ' + articleObj.title+'</h3></a></section>';
-        var impressions = '<section class="impressions">' + articleObj.shares.total + '</section>'
+        var impressions = '<section class="impressions">' + articleObj.shares.total + '</section>';
         $('#main').append(article_struct + article_pic + article_content + impressions + '<div class="clearfix"></div></article>');
       });
     },
@@ -89,12 +89,12 @@ function openReddit(){
       console.log(response.data.children[1].data.preview.images[0].source.url);
 
       $.each(response.data.children[1], function(index, articleObj){
-        var article_struct = '<article class="article">';  
+        var article_struct = '<article class="article">';
         var article_pic = '<section class="featuredImage">' +'<img src="'+articleObj.data.preview.images[0].source.url+'" alt="" />' + '</section>';
         //console.log(articleObj.data)
         $('#main').append(article_struct + article_pic + '<div class="clearfix"></div></article>');
 
-      })
+      });
     }
   });
 }
@@ -105,5 +105,5 @@ function openReddit(){
 
 
 //sample helper function
-var help = models
-help.hello("world")
+var help = models;
+help.hello("world");
