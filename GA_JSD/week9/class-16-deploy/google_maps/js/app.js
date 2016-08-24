91e0123653bfae3a16dad15cac37bf9a9ea6deed
Bijesse <tom@bijesse.com>
2016-08-23 21:28:38 -0400

    citi

--- GA_JSD/week9/class-16-deploy/google_maps/js/app.js
+++ GA_JSD/week9/class-16-deploy/google_maps/js/app.js
@@ -1,4 +1,73 @@
 $(document).ready(function() {
-})
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7128, lng: -74.0059},
+      zoom: 13
+    });
+  }
+  initMap();
+
+//Mapping cityhall and home
+  // var cityhall = new google.maps.Marker({
+  //   position: {
+  //     lat: 40.7128,
+  //     lng: -74.0059,
+  //   },
+  //   map: map,
+  //   title: "city Hall"
+  // });
+  //
+  // var home = new google.maps.Marker({
+  //   position: {
+  //     lat: 40.710315,
+  //     lng: -73.9548747,
+  //   },
+  //   map: map,
+  //   title: "home"
+  // });
+
+var citiUrl = "https://api.citybik.es/citi-bike-nyc.json";
+  $.ajax(
+   {
+      url: citiUrl,
+      type: "GET",
+      success: function (response){
+        zero(response);
+      },
+      error: function (response) {
+        console.log(response);
+      }
+
+    });
 
 
+    function zero (response){
+      response.forEach(function(item, index) {
+    var terminal = new google.maps.Marker({
+      position: {
+        lat: item.lat / 1000000,
+        lng: item.lng / 1000000,
+      },
+        map: map,
+        title: item.name
+    });
+  //Event listener for the terminal clicked
+          google.maps.event.addListener(terminal,'click',function() {
+            terminal.addListener('click', function() {
+              infowindow.open(map, terminal);
+        });
+    //map.setZoom(18);
+    //map.setCenter(terminal.getPosition());
+    });
+
+  //generate info window
+    var infowindow = new google.maps.InfoWindow({
+              content: item.lat + '',
+            });
+
+});
+
+    }
+
+});
