4354698cab0fd9b060d86afda295586ce5f0cf26
Code Club1 <codeclub1@Codes-MacBook-Air.local>
2017-05-02 17:00:08 +1200

    removed GA

--- GA_JSD/week8/class-15-crud-and-firebase/korn_app_starter_code/public/js/keys.js
+++
@@ -1,8 +0,0 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDXAnq8bnxIThMl_GMOdVOG6gPswCZmPeE",
-  authDomain: "fan-site-9a6ba.firebaseapp.com",
-  databaseURL: "https://fan-site-9a6ba.firebaseio.com",
-  storageBucket: "",
-};
-firebase.initializeApp(config);
